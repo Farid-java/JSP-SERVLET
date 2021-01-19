3a21ed1fd448a5a08534e2198abed8dd2bd89654
farid <ansari.farid1@gmail.com>
2021-01-19 16:09:29 +0530

    add entities with getter and setter , constructor

---
+++ src/main/java/com/entities/User.java
@@ -0,0 +1,85 @@
+package com.entities;
+
+import java.sql.Timestamp;
+
+public class User {
+	
+	private int id;
+	public User(int id, String name, String email, String password, String gender, Timestamp dateTime, String about) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+		this.gender = gender;
+		this.dateTime = dateTime;
+		this.about = about;
+	}
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", email=" + email + ", password=" + password + ", gender="
+				+ gender + ", dateTime=" + dateTime + ", about=" + about + "]";
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public User(String name, String email, String password, String gender, Timestamp dateTime, String about) {
+		super();
+		this.name = name;
+		this.email = email;
+		this.password = password;
+		this.gender = gender;
+		this.dateTime = dateTime;
+		this.about = about;
+	}
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public Timestamp getDateTime() {
+		return dateTime;
+	}
+	public void setDateTime(Timestamp dateTime) {
+		this.dateTime = dateTime;
+	}
+	public String getAbout() {
+		return about;
+	}
+	public void setAbout(String about) {
+		this.about = about;
+	}
+	private String name;
+	private String email;
+   private String password;
+   private String gender;
+    private Timestamp dateTime;
+   private String about;
+    
+}
